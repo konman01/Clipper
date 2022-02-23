ea5e4adea40ebf7c30f0d5c4f511ed289b989d56
konman01 <konimanjunatha@gmail.com>
2022-02-23 00:27:22 -0600

    Created Project Files

---
+++ clipper/src/test/java/com/konman/clipper/ClipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konman.clipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
