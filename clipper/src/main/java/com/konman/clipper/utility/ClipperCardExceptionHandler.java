3212a02e14c0dcb303805e65e89dcc931ff800a7
konman01 <konimanjunatha@gmail.com>
2022-03-04 01:08:55 -0600

    Create only one file to handle Exceptions for Clipper Project

--- clipper/src/main/java/com/konman/clipper/utility/ClipperCardExceptionHandler.java
+++ clipper/src/main/java/com/konman/clipper/utility/ClipperCardExceptionHandler.java
@@ -11,9 +11,46 @@ import com.konman.clipper.model.UserErrorResponse;
 
 @ControllerAdvice
 public class ClipperCardExceptionHandler {
+	
+	// Function to throw the error Response for general erro
+	@ExceptionHandler
+	public ResponseEntity<UserErrorResponse> handleException(Exception exception) {
+			
+		exception.printStackTrace();
+		UserErrorResponse error = new UserErrorResponse();
+		error.setMessage(exception.getMessage());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setTimestamp(System.currentTimeMillis());
+		ClipperUtility.clipperLogger.info("Throwing General Exception");
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+	// Exception to throw when the User is not found
+	@ExceptionHandler
+	public ResponseEntity<UserErrorResponse>  handleException(UserNotFoundException exception){
+		
+		UserErrorResponse error = new UserErrorResponse();
+		error.setMessage(exception.getMessage());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setTimestamp(System.currentTimeMillis());
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<UserErrorResponse> handleException(UserAlreadyExistException userAlreadyExistException){
+		
+		UserErrorResponse error = new UserErrorResponse();
+		error.setMessage(userAlreadyExistException.getMessage());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setTimestamp(System.currentTimeMillis());
+		ClipperUtility.clipperLogger.info("User Already Exists Exception");
+		return new ResponseEntity<UserErrorResponse>(error, HttpStatus.NOT_FOUND);
+		
+	}
 
 	
-	// Function to throw the error Response when User Not Found
+	// Function to throw When clipper card is not found
 	@ExceptionHandler
 	public ResponseEntity<ClipperCardErrorResponse>  handleException(ClipperCardException exception){
 		
